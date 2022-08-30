3abefe2e2073b2def333abd9a27f2390002cc73a
Tweety-kani <112468586+Tweety-kani@users.noreply.github.com>
2022-08-30 15:40:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.title('BREAKFAST MENU')
 streamlit.title('omega 3 &Blueberry Oatmeal')
+streamlit.title('kale, spinach & Rocket Smoothie')
+streamlit.title('Hard-Boiled Free-Range Egg')
+streamlit.title('Avacoda Toast')
